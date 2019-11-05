334a02c2989ac176cd91d2bf4f9d4960219363a0
tanakakz <tanakakz.feb.23@gmail.com>
2019-11-05 16:06:47 +0900

    turbolinksのload発生時にブラウザコンソールへログを出す

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,8 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+document.addEventListener("turbolinks:load", function() {
+  console.log("turbolinks:load fire")
+})
+
